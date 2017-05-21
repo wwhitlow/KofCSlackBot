7d610547b6fecff71555e463d5f208ad8a69b106
wwhitlow <wwhitlow@gatech.edu>
2017-05-20 22:58:40 -0400

    Figured out how to start the bots ability to reply to messages

--- rtmapi.py
+++ rtmapi.py
@@ -6,6 +6,7 @@ slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
 if sc.rtm_connect():
+    sc.rtm_send_message("D5FDFE517", "test")
     while True:
         print sc.rtm_read()
         time.sleep(1)
