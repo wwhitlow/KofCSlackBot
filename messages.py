2fc56309bba1622c67979482e90cca1e5832fd86
wwhitlow <wwhitlow@gatech.edu>
2017-05-25 16:52:33 -0400

    Added placeholder variables for Directors

--- messages.py
+++ messages.py
@@ -4,6 +4,7 @@ massMessage = "Sign up to help with mass at https://docs.google.com/spreadsheets
 #                                                                                                                                                                                           spacing here is to force new line break
 requestMessage = "This Slack bot is in continuous development. If you have an idea or believe something should be changed feel free to message @will on Slack or message the bot with keyword request and \n\"<Request here in strings>\""
 calendarMessage = "Public Calendar for all events for the Fraternal year can be found at this link https://calendar.google.com/calendar/embed?src=ekacorqf8u5djofr8egll3il4s%40group.calendar.google.com&ctz=America/New_York"
+
 whoIsGKMessage = "The Grand Knight for this Fraternal Year is Will Whitlow(@will on slack). Feel free to message him on Slack, call/text at 706-302-7470, or email at wwhitlow@gatech.edu"
 whoIsDGKMessage = "The Deputy Grand Knight for this Fraternal Year is Andrew Lewis(@alewis on slack). Feel free to message him on Slack or email at aklew12@gmail.com"
 whoIsTreasurerMessage = "The Treasurer for this Fraternal Year is Clay Newman(@clay.newman on slack). Feel free to message him on Slack or email at i don't have it :disappointed:"
@@ -16,3 +17,9 @@ whoIsOutsideGuardMessage = "The Outside Guard for this Fraternal Year is Andres 
 whoIs1stTrusteeMessage = "The 1st Year Trustee for this Fraternal Year is Andrew Renuart(@arenuart3 on slack). Feel free to message him on Slack or email at i don't have it :disappointed:"
 whoIs2ndTrusteeMessage = "The 2nd Year Trustee for this Fraternal Year is Mitchell Tuck(not on slack :disappointed:). Feel free to message him on Slack or email at i don't have it :disappointed:"
 whoIs3rdTrusteeMessage = "The 3rd Year Trustee for this Fraternal Year is Zach Basel(@zbasel12 on slack). Feel free to message him on Slack or email at i don't have it :disappointed:"
+
+whoIsProLifeDirector = "";
+whoIsChurchDirector = "";
+whoIsCommunityDirector = "";
+whoIsYouthDirector = "";
+whoIsFamilyDirector = "";
