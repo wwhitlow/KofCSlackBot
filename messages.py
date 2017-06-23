e20e8c76e869370099a14514152060ceffe87e95
wwhitlow <wwhitlow@gatech.edu>
2017-06-23 17:19:37 -0400

    Added more methods this is gonna take a while...

--- messages.py
+++ messages.py
@@ -14,18 +14,18 @@ methodDict = {}
 
 #helpMessage apparently crashes the slack client...
 helpMsg = "placeholder"
-meetingMessage = "Meetings are the First Thursday of every month at 7pm in the Classroom at the CC(if you don't know where that is start in the basement and someone should be able to help you)! Please remember to bring your Membership Card and Rosary!!!!!!!"
+meetingMsg = "Meetings are the First Thursday of every month at 7pm in the Classroom at the CC(if you don't know where that is start in the basement and someone should be able to help you)! Please remember to bring your Membership Card and Rosary!!!!!!!"
 #minutesMessage also does not work...
 minutesMsg = "Minute Meetings found at this link: "
-massMessage = "Sign up to help with mass at https://docs.google.com/spreadsheets/d/1s_lssu3mIT8ErbePxntWEJnuZR4RhUVaRMEYTUJ1R64/edit?usp=sharing The Knight's Mass is every Third Sunday of the month!"
+massMsg = "Sign up to help with mass at https://docs.google.com/spreadsheets/d/1s_lssu3mIT8ErbePxntWEJnuZR4RhUVaRMEYTUJ1R64/edit?usp=sharing The Knight's Mass is every Third Sunday of the month!"
 #                                                                                                                                                                                           spacing here is to force new line break
-requestMessage = "This Slack bot is in continuous development. If you have an idea or believe something should be changed feel free to message '@will' on Slack or message the bot with keyword request and \n\"<Request here in strings>\""
-calendarMessage = "Public Calendar for all events for the Fraternal year can be found at this link https://calendar.google.com/calendar/embed?src=ekacorqf8u5djofr8egll3il4s%40group.calendar.google.com&ctz=America/New_York"
+requestMsg = "This Slack bot is in continuous development. If you have an idea or believe something should be changed feel free to message '@will' on Slack or message the bot with keyword request and \n\"<Request here in strings>\""
+calendarMsg = "Public Calendar for all events for the Fraternal year can be found at this link https://calendar.google.com/calendar/embed?src=ekacorqf8u5djofr8egll3il4s%40group.calendar.google.com&ctz=America/New_York"
 
-joyfulMessage = "The Joyful Mysteries Are:\n1. The Anunciation\n2. The Visitation\n3. The Nativity\n4.The Presentation of Jesus in the Temple\n5. The Finding of Jesus in the Temple"
-sorrowfulMessage = "The Sorrowful Mysteries Are:\n1. The Agony of Jesus in the Garden\n2. The Sourging at the Pillar\n3. The Crowning with Thorns\n4. Jesus Carrying the Cross\n5. The Crucifixion of our Lord"
-gloriousMessage = "The Glorious Mysteries Are:\n1. The Resurrection of Jesus Christ\n2. The Acension of Jesus to Heaven\n3. The Descent of the Holy Spirit at Pentecost\n4. The Assumption of Mary into Heaven\n5. Mary is Crowned as Queen of Heaven and Earth"
-luminousMessage = "The Luminous Mysteries Are:\n1. The Baptism in the Jordan\n2. The Wedding at Cana\n3. The Proclamation of the Kingdom of God\n4. The Transiguration\n5. The Institution of the Eucharist"
+joyfulMsg = "The Joyful Mysteries Are:\n1. The Anunciation\n2. The Visitation\n3. The Nativity\n4.The Presentation of Jesus in the Temple\n5. The Finding of Jesus in the Temple"
+sorrowfulMsg = "The Sorrowful Mysteries Are:\n1. The Agony of Jesus in the Garden\n2. The Sourging at the Pillar\n3. The Crowning with Thorns\n4. Jesus Carrying the Cross\n5. The Crucifixion of our Lord"
+gloriousMsg = "The Glorious Mysteries Are:\n1. The Resurrection of Jesus Christ\n2. The Acension of Jesus to Heaven\n3. The Descent of the Holy Spirit at Pentecost\n4. The Assumption of Mary into Heaven\n5. Mary is Crowned as Queen of Heaven and Earth"
+luminousMsg = "The Luminous Mysteries Are:\n1. The Baptism in the Jordan\n2. The Wedding at Cana\n3. The Proclamation of the Kingdom of God\n4. The Transiguration\n5. The Institution of the Eucharist"
 
 THWg = "TO HELL WITH GEORGIA!!!"
 pissOnThem = "Piss on Them!!!"
@@ -55,7 +55,7 @@ sc.rtm_connect()
 def helpMessage(channel):
     sc.rtm_send_message(channel, helpMsg)
 
-#methodDict['help'] = helpMessage
+methodDict['help'] = helpMessage
 
 def minutesMessage(channel):
     sc.rtm_send_message(channel, minutesMsg)
@@ -63,28 +63,34 @@ def minutesMessage(channel):
 methodDict['minutes'] = minutesMessage
 
 def meetingMessage(channel):
-    sc.rtm_send_message(channel, meetingMessage)
+    sc.rtm_send_message(channel, meetingMsg)
+
+methodDict['meeting'] = meetingMessage
 
 def massMessage(channel):
-    sc.rtm_send_message(channel, massMessage)
+    sc.rtm_send_message(channel, massMsg)
+
+methodDict['mass'] = massMessage
 
 def requestMessage(channel):
-    sc.rtm_send_message(channel, requestMessage)
+    sc.rtm_send_message(channel, requestMsg)
+
+methodDict['request'] = requestMessage
 
 def calendarMessage(channel):
-    sc.rtm_send_message(channel, calendarMessage)
+    sc.rtm_send_message(channel, calendarMsg)
 
 def joyfulMessage(channel):
-    sc.rtm_send_message(channel, joyfulMessage)
+    sc.rtm_send_message(channel, joyfulMsg)
 
 def sorrowfulMessage(channel):
-    sc.rtm_send_message(channel, sorrowfulMessage)
+    sc.rtm_send_message(channel, sorrowfulMsg)
 
 def gloriousMessage(channel):
-    sc.rtm_send_message(channel, gloriousMessage)
+    sc.rtm_send_message(channel, gloriousMsg)
 
 def luminousMessage(channel):
-    sc.rtm_send_message(channel, luminousMessage)
+    sc.rtm_send_message(channel, luminousMsg)
 
 def THWgMsg(channel):
     sc.rtm_send_message(channel, THWgMessage)
